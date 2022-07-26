60b7490fdb9df4b6c0f2e7286dde7773d9583546
Elayt <elaytpatters@gmail.com>
2022-07-26 16:43:19 +0500

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+let listBtn = document.getElementById('list-btn');
+let openBox = document.getElementById('open-box');
+let closeBtn = document.getElementById('closeBtn');
+let body = document.getElementsByTagName('body')[0];
+let openboxInner = document.getElementById('openbox-inner');
+
+listBtn.onclick = function() {
+  openBox.classList.remove('d-none');
+  openBox.classList.add('d-block');
+  body.classList.add('overflow-hidden');
+};
+
+closeBtn.onclick = function() {
+  openBox.classList.remove('d-block');
+  openBox.classList.add('d-none');
+  body.classList.remove('overflow-hidden');
+};
